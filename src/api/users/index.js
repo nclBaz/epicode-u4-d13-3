2d16b13dba5b9e3be4437b5fe8bc6c92b3a208ab
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-01-18 12:37:43 +0100

    Completes CRUD on purchaseHistory

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -88,6 +88,7 @@ usersRouter.post("/:userId/purchaseHistory", async (req, res, next) => {
 
     // 1. Find the book by id in the books'collection
     const purchasedBook = await BooksModel.findById(req.body.bookId, { _id: 0 })
+
     // here we could use projection ({_id: 0}) to remove the _id from the book. In this way Mongo will create automagically a unique _id for every item added to the array
 
     if (purchasedBook) {
@@ -95,7 +96,7 @@ usersRouter.post("/:userId/purchaseHistory", async (req, res, next) => {
       const bookToInsert = { ...purchasedBook.toObject(), purchaseDate: new Date() }
       // purchasedBook and EVERYTHING comes from a .find(), .findById(),.... is a MONGOOSE DOCUMENT not a normal object!
       // Therefore if you want to spread it probably you should convert it into a plain normal object
-      console.log(bookToInsert)
+
       // 3. Update the specified user by adding that book to his/her purchaseHistory array
       const updatedUser = await UsersModel.findByIdAndUpdate(
         req.params.userId, // WHO
@@ -103,7 +104,11 @@ usersRouter.post("/:userId/purchaseHistory", async (req, res, next) => {
         { new: true, runValidators: true } // OPTIONS
       )
 
-      res.send(updatedUser)
+      if (updatedUser) {
+        res.send(updatedUser)
+      } else {
+        next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+      }
     } else {
       // 4. In case of either book not found or user not found --> 404
       next(createHttpError(404, `Book with id ${req.body.bookId} not found!`))
@@ -115,6 +120,12 @@ usersRouter.post("/:userId/purchaseHistory", async (req, res, next) => {
 
 usersRouter.get("/:userId/purchaseHistory", async (req, res, next) => {
   try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      res.send(user.purchaseHistory)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
@@ -122,6 +133,23 @@ usersRouter.get("/:userId/purchaseHistory", async (req, res, next) => {
 
 usersRouter.get("/:userId/purchaseHistory/:productId", async (req, res, next) => {
   try {
+    const user = await UsersModel.findById(req.params.userId)
+    if (user) {
+      console.log(user)
+      const purchasedBook = user.purchaseHistory.find(
+        book => book._id.toString() === req.params.productId
+      )
+      // You CANNOT compare a string (req.params.productId) with an ObjectId (book._id)
+      // Solution --> you have to either convert _id into a string or productId into ObjectId
+      console.log(purchasedBook)
+      if (purchasedBook) {
+        res.send(purchasedBook)
+      } else {
+        next(createHttpError(404, `Product with id ${req.params.productId} not found!`))
+      }
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
@@ -129,6 +157,28 @@ usersRouter.get("/:userId/purchaseHistory/:productId", async (req, res, next) =>
 
 usersRouter.put("/:userId/purchaseHistory/:productId", async (req, res, next) => {
   try {
+    // 1. Find User by Id (obtaining MONGOOSE DOCUMENT!)
+    const user = await UsersModel.findById(req.params.userId)
+
+    if (user) {
+      // 2. Update the item in the array by using normal JS code
+      // 2.1 Search for the index of the product into the purchaseHistory array
+      const index = user.purchaseHistory.findIndex(
+        book => book._id.toString() === req.params.productId
+      )
+      if (index !== -1) {
+        // 2.2 If the product is there --> modify that product with some new data coming from req.body
+        user.purchaseHistory[index] = { ...user.purchaseHistory[index].toObject(), ...req.body }
+        // 3. Since user object is a MONGOOSE DOCUMENT I can use .save() method to update that record
+        await user.save()
+        res.send(user)
+      } else {
+        // 2.3 If product is not there --> 404
+        next(createHttpError(404, `Product with id ${req.params.productId} not found!`))
+      }
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
@@ -136,6 +186,16 @@ usersRouter.put("/:userId/purchaseHistory/:productId", async (req, res, next) =>
 
 usersRouter.delete("/:userId/purchaseHistory/:productId", async (req, res, next) => {
   try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId, // WHO
+      { $pull: { purchaseHistory: { _id: req.params.productId } } }, // HOW
+      { new: true } // OPTIONS
+    )
+    if (updatedUser) {
+      res.send(updatedUser)
+    } else {
+      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+    }
   } catch (error) {
     next(error)
   }
