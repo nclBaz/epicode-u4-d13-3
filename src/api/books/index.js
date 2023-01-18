10df0c2f9113db8cb6ed7921bae6830cabd0af5f
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-01-18 10:45:49 +0100

    Adds pagination

---
+++ src/api/books/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import q2m from "query-to-mongo"
+import BooksModel from "./model.js"
+
+const booksRouter = express.Router()
+
+booksRouter.get("/", async (req, res, next) => {
+  try {
+    console.log("EXPRESS QUERY:", req.query)
+    console.log("QUERY 2 MONGO:", q2m(req.query))
+    const mongoQuery = q2m(req.query)
+
+    const total = await BooksModel.countDocuments(mongoQuery.criteria)
+
+    const books = await BooksModel.find(mongoQuery.criteria, mongoQuery.options.fields)
+      .limit(mongoQuery.options.limit) // No matter the order of usage of these 3 options, Mongo will ALWAYS go with SORT, then SKIP, then LIMIT
+      .skip(mongoQuery.options.skip)
+      .sort(mongoQuery.options.sort)
+    res.send({
+      links: mongoQuery.links("http://localhost:3001/books", total),
+      totalPages: Math.ceil(total / mongoQuery.options.limit),
+      books,
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default booksRouter
